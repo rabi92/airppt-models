99a8da4603a0b209db07bafd38c59aedc4f7cf55
Rabi <rabi.ulislam92@gmail.com>
2022-08-15 15:31:13 +0400

    add types for ppt section

--- pptdetails.d.ts
+++ pptdetails.d.ts
@@ -2,12 +2,11 @@ import { PowerpointElement } from "./pptelement";
 export interface PowerpointDetails {
     powerPointElements: PowerpointElement[];
     inputPath: string;
+    slidesLength: number;
     sections: PptSection[];
 }
-
 export interface PptSection {
-    title: string,
-    startingSlide: number,
-    lastSlide: number
-    slidesLength: number;
+    title: string;
+    startingSlide: number;
+    lastSlide: number;
 }
